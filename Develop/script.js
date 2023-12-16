785d0119ba845be4b40d8e284da147a60d63e428
Ash <ashloraptor@gmail.com>
2023-12-16 14:43:31 -0500

    Added code to commit user-entered data to the local storage, and display information saved to local storage within hour block description.

--- Develop/script.js
+++ Develop/script.js
@@ -6,8 +6,7 @@
 container = document.querySelector("#currentDay");
 
 var currentHour = dayjs().hour();
-var today = dayjs();
-$('#currentDay').text(today);
+
 
 $(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -34,10 +33,6 @@ $(document).ready(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-
-
-  
-
   //loops through time blocks
   $('.time-block').each(function () {
     var blockHour = parseInt($(this).attr('id').split('-')[1]);
@@ -61,7 +56,17 @@ $(document).ready(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+ $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+ $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+ $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+ $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+ $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+ $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+ $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+ $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+ $('#hour-17 .description').val(localStorage.getItem('hour-17'));
   //
   // TODO: Add code to display the current date in the header of the page.
-
+  var today = dayjs();
+  $('#currentDay').text(today);
 });
